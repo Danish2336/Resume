3863384b9dea826725f23ffff9c7c2f7bf68d5ff
Danish Raza <razad431@gmail.com>
2025-03-14 14:34:12 +0500

    First Push

---
+++ resume/src/components/Main.jsx
@@ -0,0 +1,135 @@
+import profileImg from "../assets/danish.jpg";
+export const Main = () => {
+    return(
+    <div className= "flex w-230 h-full justify-self-center  mt-6  border mb-4">
+        <div className="w-1/3  bg-[#163853]  ">
+        <div className="relative flex justify-center my-10 ">
+        <img src={profileImg} alt="Profile"
+                className="absolute mt-23 transform -translate-y-1/2 w-44 h-44 rounded-full border-4 border-white" />
+        </div>
+
+        <div className="px-10 py-2 mt-48 ">
+        <h2 className="text-2xl font-bold text-white">CONTACT</h2>
+        <hr className="mt-1 border-white"/>
+        <h4 className="text-white font-small mt-2 px-2"><strong> Phone:</strong> +92 312-0786543</h4>
+        <h4 className="text-white font-small mt-2 px-2"><strong>Email:</strong> razad431@gmail.com</h4>
+        <h4 className="text-white font-small mt-2 text-justify px-2"><strong> Address:</strong> House no 20, Street no 7,Haji taj din street chohan park, islampura, Lahore</h4>
+        </div>
+
+        <div className="px-10 mt-5">
+        <h2 className="text-2xl font-bold text-white">EDUCATION</h2>
+        <hr className="mt-1 border-white"/>
+        <h3 className="text-white font-medium mt-2 px-2">2020-2024</h3>
+        <h3 className="text-white font-medium  px-2">BS.Computer Science </h3>
+        <h4 className="text-white font-small mt-1.5 px-2">University of Engineering and Technology ,Lahore</h4>
+
+        <h3 className="text-white font-medium mt-4 px-2">2018-2020</h3>
+        <h3 className="text-white font-medium  px-2">FSc.Pre Engineering</h3>
+        <h4 className="text-white font-small mt-1.5 px-2">University of Engineering and Technology ,Lahore</h4>
+        </div>
+
+        <div className="px-10 mt-5">
+        <h2 className="text-2xl font-bold text-white">SKILLS</h2>
+        <hr className="mt-1 border-white"/>
+        <li className="text-white mt-2 ">Full-Stack Development</li>
+        <p className="text-white px-8">- React.Js <br /> - Codeigniter 4 <br />- PHP <br />-Tailwind CSS <br />- Bootstrap 5 <br />-MySql <br />-Firebase</p>
+        <li className="text-white mt-3">UI/UX</li>
+        <p className="text-white px-8">- Figma <br />- Canva</p>
+        <li className="text-white mt-3">Teamwork</li>
+        <li className="text-white mt-2"> Effective Communication</li>
+        <li className="text-white mt-2"> Leadership</li>
+        </div>
+
+        <div className="px-10 mt-5">
+        <h2 className="text-2xl font-bold text-white">LANGUAGES</h2>
+        <hr className="mt-1 border-white"/>
+        <li className="text-white mt-2">English</li>
+        <li className="text-white mt-2">Urdu</li>
+        </div>
+            
+        </div>
+       
+
+        <div className="w-2/3 h-full ">
+        <div className="justify-center text-center my-12 p-8">
+        <h2 className="text-4xl font-bold text-[#545454]">MUHAMMAD DANISH RAZA</h2>
+        <h3 className="text-2xl font-semibold text-[#545454] ">Full-STACK DEVELOPER</h3>
+        </div>
+
+        <div className="px-10 py-2">
+        <h2 className="text-2xl font-bold text-[#163853]">PROFILE</h2>
+        <hr className="mt-1"/>
+        <p className="text-justify mt-3 ">
+        Dynamic web developer with expertise in frontend and backend technologies. Committed to delivering innovative solutions through collaborative teamwork and staying updated with the latest industry trends. Proven track record of creating efficient and scalable web applications.</p>
+        </div>
+
+        <div className=" px-10 mt-8">
+        <h2 className="text-2xl font-bold text-[#163853]">WORK EXPERIENCE</h2>
+        <hr className="mt-1"/>
+        <div className="grid grid-rows-1 grid-cols-2 mt-4 "> 
+            <div>
+            <h3 className="text-xl font-semibold text-[#163853] ">MINDBRIDGE</h3>
+            <h2 className="font-semibold text-[#163853]">ASSOSIATE SOFTWARE DEVELOPER.</h2>
+            </div>
+            <div>
+            <h3 className="text-xl font-semibold text-right text-[#163853]">2024 (Present)</h3>
+            </div>
+       
+       
+        </div>
+        <div className="px-4 mt-4  "> 
+            <li className="text-justify">Developed responsive web applications using  CI4 and REACT frameworks.</li>
+            <li className="text-justify">Actively developing expertise in full-stack web development.</li>
+            <li className="text-justify">Strong foundation in JS, TailwindCSS, PHP.</li>
+            <li className="text-justify">Integrated RESTful APIs, ensuring seamless communication between systems.</li>
+        </div>
+
+        <div className="grid grid-rows-1 grid-cols-2 mt-12 "> 
+            <div>
+            <h3 className="text-xl font-semibold text-[#163853] ">FIVERR</h3>
+            <h2 className="font-semibold text-[#163853]">LEAD GENERATION.</h2>
+            </div>
+            <div>
+            <h3 className="text-xl font-semibold text-right text-[#163853]">2023</h3>
+            </div>
+       
+       
+        </div>
+        <div className="px-4 mt-4  "> 
+            <li className="text-justify"><strong className="text-[#163853]">Email Marketing Campaigns </strong> – generate leads, nurture prospects, and drive engagement.</li>
+            <li className="text-justify"><strong className="text-[#163853]">Data Mining & Research </strong>– Gathered and analyzed potential leads from various social media platforms.</li>
+            <li className="text-justify"><strong className="text-[#163853]">Lead Qualification & Outreach</strong> – Identified and qualified potential leads through data enrichment.</li>
+        </div>
+
+        <div className="grid grid-rows-1 grid-cols-2 mt-12 "> 
+            <div>
+            <h3 className="text-xl font-semibold text-[#163853] ">PNY INSTITUTE</h3>
+            <h2 className="font-semibold text-[#163853]">FULL STACK APP DEVELOPER.</h2>
+            </div>
+            <div>
+            <h3 className="text-xl font-semibold text-right text-[#163853]">2022</h3>
+            </div>
+       
+       
+        </div>
+        <div className="px-4 mt-4  "> 
+            <li className="text-justify">Integrated third-party APIs and libraries to enhance app functionality.</li>
+            <li className="text-justify">Collaborated with teams to deliver high-quality solutions.</li>
+            <li className="text-justify">Used Java and C sharp to build App.</li>
+            <li className="text-justify">Conducted thorough testing and debugging to ensure app stability and reliability.</li>
+        </div>
+       
+        <div className=" px-2 py-3 ">
+        <h2 className="text-2xl font-bold text-[#163853]">PROJECTS</h2>
+        <hr className="mt-1"/>
+        <li className="mt-2">PUNJAB COMPLAINT SYSTEM FOR WOMEN(PCSW) - CI4</li>
+        <li>QR Code Generator with Vcard - CI4 </li>
+        <li>Locker Managment - CI4 </li>
+        <li>Personal Portfolio Website - React, Tailwind CSS</li>
+        <li>Fitness Management App - JAVA,C#</li>
+        </div>
+        </div>
+        </div>
+    </div>
+    );
+}
